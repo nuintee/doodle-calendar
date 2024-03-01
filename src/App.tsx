bdaccadef7bb3f9701d5fa09ec33e0d09858cfa8
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-02 01:13:10 +0900

    App.tsxのスタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-const OPTIONS = ["📈 SquadBeyond", "👨‍⚕️ TEN"];
+import { useState } from "react";
 
 const sendMessage = (value: string) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -13,11 +13,46 @@ const sendMessage = (value: string) => {
 };
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [templates, setTemplates] = useState<string[]>([]);
+
+  const addTemplates = () => {
+    if (!inputValue) return;
+
+    setTemplates((prev) => [
+      ...prev.filter((value) => value !== inputValue),
+      inputValue,
+    ]);
+  };
+
   return (
-    <div className="flex-col">
-      {OPTIONS.map((option) => (
-        <button onClick={() => sendMessage(option)}>{option}</button>
-      ))}
+    <div className="flex flex-col gap-y-2 p-2 w-[280px]">
+      <div className="flex items-center gap-x-1">
+        <input
+          type="text"
+          className="flex-1 block outline-none"
+          placeholder="ラベル名を入力して下さい"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          className="px-4 py-2 bg-blue-500 rounded-md text-white"
+          onClick={addTemplates}
+        >
+          保存
+        </button>
+      </div>
+      <div className="bg-gray-300 w-full h-px"></div>
+      <div>
+        {templates.map((template) => (
+          <button
+            className="w-full px-4 py-2 text-start hover:bg-gray-200 rounded-md"
+            onClick={() => sendMessage(template)}
+          >
+            {template}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
