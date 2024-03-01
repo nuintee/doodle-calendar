3fe7b7ad55ee68870ea2e2f8297ec1c6fb379e3e
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-01 23:54:20 +0900

    selector変更時にinputを変える処理の追加

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,10 +1,32 @@
+const OPTIONS = ["📈 SquadBeyond", "👨‍⚕️ TEN"];
+
 const insertedNodes: Node[] = [];
 const observer = new MutationObserver(function () {
   const input = document.querySelector('input[aria-label="タイトルを追加"]');
 
   if (!input) return;
 
-  (input as HTMLInputElement).value = `💡 Test`;
+  const selector = document.createElement("select");
+  OPTIONS.forEach((value) => {
+    const option = document.createElement("option");
+    option.value = value;
+    option.innerHTML = value;
+
+    selector.appendChild(option);
+  });
+
+  const parentLabel = input.parentElement;
+
+  if (parentLabel) {
+    parentLabel.style.paddingLeft = "1rem";
+  }
+
+  selector.onchange = (e) =>
+    ((input as HTMLInputElement).value = (
+      e?.target as HTMLSelectElement
+    )?.value);
+
+  parentLabel?.appendChild(selector);
 });
 observer.observe(document.documentElement, { childList: true });
 console.log(insertedNodes);
@@ -13,8 +35,6 @@ chrome.runtime.onMessage.addListener(function (request) {
   const value = JSON.parse(request)?.value;
 
   const input = document.querySelector('input[aria-label="タイトルを追加"]');
-
-  console.log({ input, value });
 
   if (!input || !value) return;
 
