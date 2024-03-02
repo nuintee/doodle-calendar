0343967af3d9d740e1a7e3be8a420ce05d062aec
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-02 22:24:51 +0900

    データ追加時の同期追加

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,11 +1,18 @@
+const SELECTOR_ID = "__TEMPLATE_SELECTOR";
+
 const getInputDOM = () => {
   return document.querySelector(
     'input[aria-label="タイトルを追加"]'
   ) as HTMLInputElement | null;
 };
 
+const getSelector = () => {
+  return document.getElementById(SELECTOR_ID) as HTMLSelectElement | null;
+};
+
 const createSelector = (options: string[]) => {
   const selector = document.createElement("select");
+  selector.id = SELECTOR_ID;
 
   options.forEach((value) => {
     const option = document.createElement("option");
@@ -24,6 +31,8 @@ const createSelector = (options: string[]) => {
     input.value = `${target.value} ${input.value}`;
   };
 
+  console.log({ selector });
+
   return selector;
 };
 
@@ -41,6 +50,28 @@ const observer = new MutationObserver(async function () {
 });
 
 observer.observe(document.documentElement, { childList: true });
+
+chrome.storage.local.onChanged.addListener(({ T }) => {
+  const selector = getSelector();
+
+  if (!T.newValue) return selector?.remove();
+
+  if (!selector) {
+    const newSelector = createSelector(T.newValue);
+    const input = getInputDOM();
+    const parentLabel = input?.parentElement;
+    return parentLabel?.appendChild(newSelector);
+  }
+
+  selector?.replaceChildren();
+  T.newValue?.forEach((value: string) => {
+    const option = document.createElement("option");
+    option.value = value;
+    option.innerHTML = value;
+
+    selector?.append(option);
+  });
+});
 
 chrome.runtime.onMessage.addListener(function (request) {
   const value = JSON.parse(request)?.value;
