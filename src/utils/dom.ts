7aa626e7ba62919cbc62d0e99c792997871a3b3d
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-02 23:52:43 +0900

    selectColor関数の追加

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,5 @@
+import { CALENDAR_COLORS, ColorHex, ColorLabel } from "../constants/colors";
+
 const SELECTOR_ID = "template-selector";
 
 export const getInputDOM = () => {
@@ -44,4 +46,43 @@ export const setSelector = (options: string[]) => {
   options?.forEach((value: string) => {
     selector?.append(createOption(value));
   });
+};
+
+export const setColor = (key: ColorLabel | ColorHex) => {
+  const tabPanel = document.querySelector('div[role="tabpanel"]');
+  const tabPanelSpan = tabPanel?.querySelector("span");
+
+  const allExpandables = tabPanelSpan?.querySelectorAll("div[data-expandable]");
+  const colorSection = allExpandables?.item(allExpandables.length - 1);
+  const colorSectionTrigger = colorSection?.querySelector("button");
+
+  // 色セクションを開く
+  colorSectionTrigger?.click();
+
+  const colorTrigger = colorSection?.querySelector(
+    `div[id] > 
+     div > 
+     div:first-child >
+     div >
+     div[data-dragsource-ignore="true"] >
+     div:last-child >
+     div:last-child >
+     div:first-child >
+     span >
+     button
+    `
+  ) as HTMLButtonElement | null | undefined;
+
+  // 色ポップアップ表示ボタンのクリック
+  colorTrigger?.click();
+
+  const colorButtons = document.querySelectorAll(
+    'div[data-color][role="menuitemradio"]'
+  );
+
+  const colorIndex = CALENDAR_COLORS.findIndex(
+    (color) => color.label === key || color.hex === key
+  );
+
+  (colorButtons[colorIndex] as HTMLButtonElement).click();
 };
