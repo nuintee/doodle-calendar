0f2d3718e0ea7cdf4d3d57e6abde0b6235b9074e
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-03 00:25:51 +0900

    ColorPickerコンポーネントの実装

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,37 @@ import {
   getStorageTemplates,
 } from "./utils/storage";
 import { sendMessage } from "./utils/message";
+import { CALENDAR_COLORS } from "./constants/colors";
+
+const ColorPicker = () => {
+  const [colorState, setColorState] = useState(
+    CALENDAR_COLORS.slice(0).pop()?.hex
+  );
+
+  return (
+    <details className="relative p-2 hover:bg-gray-100 rounded-md rounded-md">
+      <summary className="marker:hidden list-none">
+        <div
+          className="size-5 rounded-full"
+          style={{ backgroundColor: colorState }}
+        ></div>
+      </summary>
+      <div className="absolute w-[100px] p-2 right-0 grid grid-cols-2 gap-2 bg-white border">
+        {CALENDAR_COLORS.map((color) => (
+          <button
+            className="hover:bg-gray-100 p-2 rounded-md"
+            onClick={() => setColorState(color.hex)}
+          >
+            <div
+              className="size-5 rounded-full"
+              style={{ backgroundColor: color.hex }}
+            ></div>
+          </button>
+        ))}
+      </div>
+    </details>
+  );
+};
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -45,7 +76,7 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col gap-y-2 p-2 w-[280px]">
+    <div className="flex flex-col gap-y-2 p-2 w-[350px]">
       <div className="flex items-center gap-x-1">
         <input
           type="text"
@@ -54,8 +85,9 @@ function App() {
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
+        <ColorPicker />
         <button
-          className="px-4 py-2 bg-blue-500 rounded-md text-white"
+          className="px-4 py-2 bg-blue-500 rounded-md text-white shrink-0"
           onClick={addTemplates}
         >
           保存
