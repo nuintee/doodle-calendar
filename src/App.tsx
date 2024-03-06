d06c183558565e630f2146e49f4bafc8f1ec8e06
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-07 00:01:31 +0900

    コンテクストメニューの項目追加

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import { getCalendarColor } from "./utils/colors";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 import { InformationCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
 
+import { version } from "../package.json";
+
 function App() {
   const [inputValue, setInputValue] = useState<DecorationTemplate>({
     hex: getCalendarColor("グラファイト"),
@@ -71,7 +73,7 @@ function App() {
       <div className="flex items-center gap-x-1">
         <input
           type="text"
-          className="flex-1 block outline-none"
+          className="flex-1 text-base caret-gray-500 block outline-none"
           placeholder="ラベル名を入力して下さい"
           value={inputValue.label}
           onKeyDown={(e) => e.key === "Enter" && addTemplates()}
@@ -93,33 +95,27 @@ function App() {
           </summary>
           <div className="absolute w-[200px] p-2 right-0 flex flex-col gap-y-2 bg-white border border-gray-100 shadow-sm rounded-md">
             <button
-              className="px-4 flex items-center gap-x-1 text-start py-2 enabled:hover:bg-gray-100 text-gray-500 rounded-md shrink-0"
+              className="p-2 flex items-center gap-x-1 text-start enabled:hover:bg-gray-100 text-gray-500 rounded-md shrink-0"
               onClick={() => {}}
             >
               <InformationCircleIcon height={24} width={24} />
               使い方
             </button>
             <button
-              className="px-4 flex items-center gap-x-1 text-start py-2 enabled:hover:bg-red-100 text-red-500 rounded-md shrink-0"
+              className="p-2 flex items-center gap-x-1 text-start enabled:hover:bg-red-100 text-red-500 rounded-md shrink-0"
               onClick={clearData}
               disabled={!templates.length}
             >
               <TrashIcon height={24} width={24} />
               全テンプレートの削除
             </button>
+            <p className="px-4 py-2 w-full text-center">
+              バージョン: {version}
+            </p>
           </div>
         </details>
-        {/* {Boolean(templates.length) && (
-          <button
-            className="px-4 py-2 enabled:hover:bg-red-100 rounded-md text-white shrink-0"
-            onClick={clearData}
-            title="全テンプレートの削除"
-            aria-label="全テンプレートの削除"
-          >
-            🗑️
-          </button>
-        )} */}
       </div>
+      <span className="text-xs text-gray-500">保存済みテンプレート</span>
       <div className="bg-gray-300 w-full h-px"></div>
       <div className="flex flex-col gap-y-2 overflow-auto">
         {templates.map((template) => (
