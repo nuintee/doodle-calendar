f35562f5fd75daebb4ab5eeacb6589f00764933c
Nuintee <nuintee@ShonoMacBook-Pro.local>
2024-03-10 23:13:45 +0900

    meta・description対応

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,17 @@ import { crx, defineManifest } from '@crxjs/vite-plugin';
 const manifest = defineManifest({
   manifest_version: 3,
   name: 'Doodle Calendar',
-  description: 'Googleカレンダーの予定作成テンプレート管理',
+  description: 'Googleカレンダーの予定テンプレート管理',
   version: '1.0',
   permissions: ['activeTab', 'scripting', 'storage'],
   action: {
     default_popup: 'index.html'
+  },
+  icons: {
+    '16': 'icons/icon16.png',
+    '32': 'icons/icon32.png',
+    '48': 'icons/icon48.png',
+    '128': 'icons/icon128.png'
   },
   content_scripts: [
     {
